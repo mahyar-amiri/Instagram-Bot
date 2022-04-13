f4123122bb47492534e0219c73dba2b85574f15b
Mahyar Amiri <mmaahhyyaarr@gmail.com>
2022-04-13 10:47:29 +0430

    update ig_bot.py

--- ig_bot.py
+++ ig_bot.py
@@ -12,9 +12,27 @@ bot = telebot.TeleBot(TOKEN)
 
 client = Client()
 
+EMOJI = {
+    'username': '👤',
+    'name': '🗣️',
+    'bio': '📜',
+    'post': '🖼️',
+    'follower': '👥',
+    'following': '🫂',
+    'private': '🔒',
+    'public': '🔓',
+    'verified': '✅',
+    'notverified': '❎',
+    'url': '🌐',
+    'video': '🎞️',
+    'photo': '🖼️',
+    'notsupport': '❌',
+}
+
 is_authenticated = False
 while not is_authenticated:
     is_authenticated = client.login(config('IG_USERNAME'),  config('IG_PASSWORD'))
+
 
 bot.send_message(chat_id, f"Hello {config('IG_USERNAME')}")
 
@@ -35,19 +53,34 @@ def user_story_markup(stories):
     c = 0
 
     for i in range(count//5):
-        markup.add(InlineKeyboardButton(f'{stories[c].media_type}-{c+1}', callback_data=f'cb_story_{c}'),
-                   InlineKeyboardButton(f'{stories[c+1].media_type}-{c+2}', callback_data=f'cb_story_{c+1}'),
-                   InlineKeyboardButton(f'{stories[c+2].media_type}-{c+3}', callback_data=f'cb_story_{c+2}'),
-                   InlineKeyboardButton(f'{stories[c+3].media_type}-{c+4}', callback_data=f'cb_story_{c+3}'),
-                   InlineKeyboardButton(f'{stories[c+4].media_type}-{c+5}', callback_data=f'cb_story_{c+4}'),
-                   row_width=5)
-        c += 5
+        row = []
+        for j in range(5):
+            media_type = ''
+            if stories[c].media_type == 1:
+                media_type = EMOJI['photo']
+            elif stories[c].media_type == 2:
+                media_type = EMOJI['video']
+            else:
+                media_type = EMOJI['notsupport']
+
+            row.append(InlineKeyboardButton(f'{c+1} {media_type}', callback_data=f'cb_story_dl_{c}'))
+            c += 1
+
+        markup.add(*row, row_width=5)
 
     if count % 5 != 0:
         last_row = []
         for i in range(count % 5):
+            media_type = ''
+            if stories[c].media_type == 1:
+                media_type = EMOJI['photo']
+            elif stories[c].media_type == 2:
+                media_type = EMOJI['video']
+            else:
+                media_type = EMOJI['notsupport']
+
+            last_row.append(InlineKeyboardButton(f'{c+1} {media_type}', callback_data=f'cb_story_dl_{c}'))
             c += 1
-            last_row.append(InlineKeyboardButton(f'{c}', callback_data=f'cb_story_{c}'))
         markup.add(*last_row, row_width=count % 5)
 
     markup.add(InlineKeyboardButton('Get all', callback_data='cb_story_all'))
@@ -58,13 +91,33 @@ def user_story_markup(stories):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     if call.data == 'cb_story':
-        bot.answer_callback_query(call.id, 'Select Story')
 
         caption = call.message.caption
         user_id = caption[caption.find('#u')+2:]
         stories = client.user_stories(user_id)
 
-        bot.edit_message_reply_markup(chat_id=call.from_user.id, message_id=call.message.id, reply_markup=user_story_markup(stories))
+        if len(stories) == 0:
+            bot.answer_callback_query(call.id, 'User has no story!')
+        else:
+            bot.answer_callback_query(call.id, 'Select Story')
+            bot.edit_message_reply_markup(chat_id=call.from_user.id, message_id=call.message.id, reply_markup=user_story_markup(stories))
+
+    elif call.data.startswith('cb_story_dl_'):
+        number = int(call.data.replace('cb_story_dl_', ''))
+
+        bot.answer_callback_query(call.id, f'Get Story {number+1}!')
+
+        caption = call.message.caption
+        user_id = caption[caption.find('#u')+2:]
+        stories = client.user_stories(user_id)
+
+        story = stories[number]
+        if story.media_type == 1:
+            bot.send_photo(call.from_user.id, story.thumbnail_url)
+        elif story.media_type == 2:
+            bot.send_video(call.from_user.id, story.video_url)
+        else:
+            bot.send_message(call.from_user.id, 'FORMAT NOT FOUND!')
 
     elif call.data == 'cb_story_all':
         bot.answer_callback_query(call.id, 'Get all Stories!')
@@ -101,7 +154,18 @@ def answer_message(message):
     try:
         user_info = client.user_info_by_username(message.text)
         bot.send_photo(message.chat.id, user_info.profile_pic_url,
-                       f'Username: {user_info.username}\n\nFullName: {user_info.full_name}\n\nBiography: {user_info.biography}\n\nPosts: {user_info.media_count}\n\nFollowers: {user_info.follower_count}\n\nFollowings: {user_info.following_count}\n\n#u{user_info.pk}', reply_markup=user_info_markup(user_info.username))
+                       f'''
+                       {EMOJI['username']} Username: {user_info.username}
+                       {EMOJI['name']} FullName: {user_info.full_name}
+                       {EMOJI['private'] if user_info.is_private else EMOJI['public']}Privacy: {'Private' if user_info.is_private else 'Public'}
+                       {EMOJI['verified'] if user_info.is_verified else EMOJI['notverified']} Verify: {'Verified' if user_info.is_verified else 'Not Verified'}
+                       {EMOJI['bio']} Biography: {user_info.biography}
+                       {EMOJI['post']} Posts: {user_info.media_count}
+                       {EMOJI['follower']} Followers: {user_info.follower_count:,}
+                       {EMOJI['following']} Followings: {user_info.following_count:,}
+                       {EMOJI['url']} URL: {user_info.external_url}
+                       #u{user_info.pk}
+                       '''.replace('                       ', '\n'), reply_markup=user_info_markup(user_info.username))
     except instagrapi.exceptions.UserNotFound as e:
         bot.reply_to(message, 'User not found :(')
 
